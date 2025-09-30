82f02dee5dab4d9ff2ebf5a2e7c121a9f40af461
Krishna Priya <blacktheta20@gmail.com>
2025-09-30 22:56:08 +0530

    Format code style and clean imports in registry.py (2025-09-30)

--- src/ml_algorithms/registry.py
+++ src/ml_algorithms/registry.py
@@ -50,4 +50,4 @@ def list_categories():
 def get_algorithms(category):
     return machine_learning_algorithms.get(category, [])
 
-# Revision commit 2025-09-30 #17
+# Revision commit 2025-09-30 #18
