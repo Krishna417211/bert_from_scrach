8cf1d9c682852c219b65a6af81f890effbca62f7
Krishna Priya <blacktheta20@gmail.com>
2025-10-19 19:58:50 +0530

    Improve hyperparameter handling in datasets.py (2025-10-19)

--- src/ml_algorithms/utils/datasets.py
+++ src/ml_algorithms/utils/datasets.py
@@ -38,4 +38,4 @@ def generate_transaction_data(n_transactions=50, n_items=8, random_state=42):
         transactions.append(basket)
     return transactions
 
-# Revision commit 2025-10-19 #17
+# Revision commit 2025-10-19 #18
