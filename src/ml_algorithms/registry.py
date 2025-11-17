e4ad7681752a2572c0b516e84b76d40ede7897f4
Krishna Priya <blacktheta20@gmail.com>
2025-11-17 19:54:10 +0530

    Fix edge case handling in registry.py (2025-11-17)

--- src/ml_algorithms/registry.py
+++ src/ml_algorithms/registry.py
@@ -50,4 +50,4 @@ def list_categories():
 def get_algorithms(category):
     return machine_learning_algorithms.get(category, [])
 
-# Revision commit 2025-11-17 #16
+# Revision commit 2025-11-17 #17
