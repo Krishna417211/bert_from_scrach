c8f243f3e13f63ee0a0365afa93589a9a7a9ac65
Krishna Priya <blacktheta20@gmail.com>
2025-12-18 12:06:37 +0530

    Initial setup for linear.py (2025-12-18)

--- src/ml_algorithms/regression/linear.py
+++ src/ml_algorithms/regression/linear.py
@@ -48,4 +48,4 @@ class PolynomialRegressionFromScratch:
         X_poly = self._transform(X)
         return self.linear_model.predict(X_poly)
 
-# Revision commit 2025-12-17 #14
+# Revision commit 2025-12-18 #1
