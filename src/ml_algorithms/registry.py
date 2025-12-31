1411dbb908d625cfb8abc50e723b4477d605c53d
Krishna Priya <blacktheta20@gmail.com>
2025-12-31 16:42:08 +0530

    Enhance mathematical stability for registry.py (2025-12-31)

--- src/ml_algorithms/registry.py
+++ src/ml_algorithms/registry.py
@@ -50,4 +50,4 @@ def list_categories():
 def get_algorithms(category):
     return machine_learning_algorithms.get(category, [])
 
-# Revision commit 2025-12-31 #13
+# Revision commit 2025-12-31 #14
