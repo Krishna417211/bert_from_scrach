8ed7b9599ffe308c307962b335d8fd62c33612af
Krishna Priya <blacktheta20@gmail.com>
2026-01-18 11:02:18 +0530

    Enhance mathematical stability for linear_classifiers.py (2026-01-18)

--- src/ml_algorithms/classification/linear_classifiers.py
+++ src/ml_algorithms/classification/linear_classifiers.py
@@ -60,4 +60,4 @@ class LinearSVMFromScratch:
         approx = np.dot(X, self.w) - self.b
         return np.where(np.sign(approx) <= 0, 0, 1)
 
-# Revision commit 2026-01-17 #5
+# Revision commit 2026-01-18 #1
