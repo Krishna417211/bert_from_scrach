fce3a446765bf320271c56c3be521f2d4556cda3
Krishna Priya <blacktheta20@gmail.com>
2026-01-20 09:09:04 +0530

    Update synthetic test validation for linear.py (2026-01-20)

--- src/ml_algorithms/regression/linear.py
+++ src/ml_algorithms/regression/linear.py
@@ -48,4 +48,4 @@ class PolynomialRegressionFromScratch:
         X_poly = self._transform(X)
         return self.linear_model.predict(X_poly)
 
-# Revision commit 2026-01-19 #7
+# Revision commit 2026-01-20 #2
