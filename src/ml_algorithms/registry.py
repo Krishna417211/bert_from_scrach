217357c695ca9ed7cab29a70f8c2f724b732bccc
Krishna Priya <blacktheta20@gmail.com>
2026-01-28 14:40:53 +0530

    Implement core logic in registry.py (2026-01-28)

--- src/ml_algorithms/registry.py
+++ src/ml_algorithms/registry.py
@@ -50,4 +50,4 @@ def list_categories():
 def get_algorithms(category):
     return machine_learning_algorithms.get(category, [])
 
-# Revision commit 2026-01-28 #10
+# Revision commit 2026-01-28 #13
