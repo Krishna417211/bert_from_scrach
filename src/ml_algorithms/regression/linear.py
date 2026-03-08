2ea337242e917afc2378e9e8627fd0d5d2f01c91
Krishna Priya <blacktheta20@gmail.com>
2026-03-08 13:26:38 +0530

    Optimize NumPy matrix operations for linear.py (2026-03-08)

--- src/ml_algorithms/regression/linear.py
+++ src/ml_algorithms/regression/linear.py
@@ -48,4 +48,4 @@ class PolynomialRegressionFromScratch:
         X_poly = self._transform(X)
         return self.linear_model.predict(X_poly)
 
-# Revision commit 2026-03-06 #13
+# Revision commit 2026-03-08 #7
