c47c29310d3be867fa7f3ccbe0759cf4da9802cc
Krishna Priya <blacktheta20@gmail.com>
2026-03-10 15:30:58 +0530

    Fix edge case handling in tree_based.py (2026-03-10)

--- src/ml_algorithms/classification/tree_based.py
+++ src/ml_algorithms/classification/tree_based.py
@@ -113,4 +113,4 @@ class RandomForestFromScratch:
         tree_preds = np.swapaxes(predictions, 0, 1)
         return np.array([np.bincount(p).argmax() for p in tree_preds])
 
-# Revision commit 2026-03-08 #3
+# Revision commit 2026-03-10 #5
