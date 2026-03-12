d9f4740daf46914822c58bed09cc2dda9b012f2f
Krishna Priya <blacktheta20@gmail.com>
2026-03-12 15:06:05 +0530

    Update synthetic test validation for tree_based.py (2026-03-12)

--- src/ml_algorithms/classification/tree_based.py
+++ src/ml_algorithms/classification/tree_based.py
@@ -113,4 +113,4 @@ class RandomForestFromScratch:
         tree_preds = np.swapaxes(predictions, 0, 1)
         return np.array([np.bincount(p).argmax() for p in tree_preds])
 
-# Revision commit 2026-03-11 #13
+# Revision commit 2026-03-12 #2
