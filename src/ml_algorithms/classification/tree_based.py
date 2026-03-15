7c79788f18e1dd81c0b088f1ed5f82fa72396e10
Krishna Priya <blacktheta20@gmail.com>
2026-03-15 16:52:34 +0530

    Add docstrings and usage instructions in tree_based.py (2026-03-15)

--- src/ml_algorithms/classification/tree_based.py
+++ src/ml_algorithms/classification/tree_based.py
@@ -113,4 +113,4 @@ class RandomForestFromScratch:
         tree_preds = np.swapaxes(predictions, 0, 1)
         return np.array([np.bincount(p).argmax() for p in tree_preds])
 
-# Revision commit 2026-03-14 #2
+# Revision commit 2026-03-15 #12
