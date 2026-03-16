78f01e98888cc36601d3feb7161bbc91c9131570
Krishna Priya <blacktheta20@gmail.com>
2026-03-16 21:41:15 +0530

    Fix edge case handling in distance_bayes.py (2026-03-16)

--- src/ml_algorithms/classification/distance_bayes.py
+++ src/ml_algorithms/classification/distance_bayes.py
@@ -55,4 +55,4 @@ class GaussianNaiveBayesFromScratch:
         denominator = np.sqrt(2 * np.pi * var)
         return numerator / denominator
 
-# Revision commit 2026-03-16 #8
+# Revision commit 2026-03-16 #13
