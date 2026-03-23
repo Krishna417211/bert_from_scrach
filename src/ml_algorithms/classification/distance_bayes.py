a09226844bda37f864babfcee7b08a0c96921ac5
Krishna Priya <blacktheta20@gmail.com>
2026-03-23 16:42:51 +0530

    Implement core logic in distance_bayes.py (2026-03-23)

--- src/ml_algorithms/classification/distance_bayes.py
+++ src/ml_algorithms/classification/distance_bayes.py
@@ -55,4 +55,4 @@ class GaussianNaiveBayesFromScratch:
         denominator = np.sqrt(2 * np.pi * var)
         return numerator / denominator
 
-# Revision commit 2026-03-22 #5
+# Revision commit 2026-03-23 #9
