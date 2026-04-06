dfff0e91a92164de5f3f7b38bb0691ab4dd03ce7
Krishna Priya <blacktheta20@gmail.com>
2026-04-06 16:48:12 +0530

    Format code style and clean imports in tree_based.py (2026-04-06)

--- src/ml_algorithms/classification/tree_based.py
+++ src/ml_algorithms/classification/tree_based.py
@@ -113,4 +113,4 @@ class RandomForestFromScratch:
         tree_preds = np.swapaxes(predictions, 0, 1)
         return np.array([np.bincount(p).argmax() for p in tree_preds])
 
-# Revision commit 2026-04-04 #8
+# Revision commit 2026-04-06 #7
