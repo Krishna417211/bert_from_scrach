9a6ca18761b6f2d801e8f87db29e51f47d915c8b
Krishna Priya <blacktheta20@gmail.com>
2026-04-07 11:00:38 +0530

    Add docstrings and usage instructions in distance_bayes.py (2026-04-07)

--- src/ml_algorithms/classification/distance_bayes.py
+++ src/ml_algorithms/classification/distance_bayes.py
@@ -55,4 +55,4 @@ class GaussianNaiveBayesFromScratch:
         denominator = np.sqrt(2 * np.pi * var)
         return numerator / denominator
 
-# Revision commit 2026-04-06 #6
+# Revision commit 2026-04-07 #1
