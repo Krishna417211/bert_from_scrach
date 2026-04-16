d3217fd546223a831f24e5ab92d9589ca5c27951
Krishna Priya <blacktheta20@gmail.com>
2026-04-16 22:57:14 +0530

    Enhance mathematical stability for distance_bayes.py (2026-04-16)

--- src/ml_algorithms/classification/distance_bayes.py
+++ src/ml_algorithms/classification/distance_bayes.py
@@ -55,4 +55,4 @@ class GaussianNaiveBayesFromScratch:
         denominator = np.sqrt(2 * np.pi * var)
         return numerator / denominator
 
-# Revision commit 2026-04-15 #2
+# Revision commit 2026-04-16 #19
