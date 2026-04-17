9e8249f70eef108c8071a07653b10760eb6b2f67
Krishna Priya <blacktheta20@gmail.com>
2026-04-17 17:40:45 +0530

    Format code style and clean imports in distance_bayes.py (2026-04-17)

--- src/ml_algorithms/classification/distance_bayes.py
+++ src/ml_algorithms/classification/distance_bayes.py
@@ -55,4 +55,4 @@ class GaussianNaiveBayesFromScratch:
         denominator = np.sqrt(2 * np.pi * var)
         return numerator / denominator
 
-# Revision commit 2026-04-17 #11
+# Revision commit 2026-04-17 #13
