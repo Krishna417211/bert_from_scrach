6291c83d2fb4a4f67c0b71e54029e43cf681e972
Krishna Priya <blacktheta20@gmail.com>
2026-04-18 16:53:41 +0530

    Refactor and add type hints to datasets.py (2026-04-18)

--- src/ml_algorithms/utils/datasets.py
+++ src/ml_algorithms/utils/datasets.py
@@ -38,4 +38,4 @@ def generate_transaction_data(n_transactions=50, n_items=8, random_state=42):
         transactions.append(basket)
     return transactions
 
-# Revision commit 2026-04-18 #13
+# Revision commit 2026-04-18 #16
