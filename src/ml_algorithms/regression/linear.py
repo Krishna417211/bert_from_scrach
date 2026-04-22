f6d4be214b34d4cc51964b47616d5f31c4aaf153
Krishna Priya <blacktheta20@gmail.com>
2026-04-22 10:13:41 +0530

    Enhance mathematical stability for linear.py (2026-04-22)

--- src/ml_algorithms/regression/linear.py
+++ src/ml_algorithms/regression/linear.py
@@ -48,4 +48,4 @@ class PolynomialRegressionFromScratch:
         X_poly = self._transform(X)
         return self.linear_model.predict(X_poly)
 
-# Revision commit 2026-04-20 #5
+# Revision commit 2026-04-22 #3
