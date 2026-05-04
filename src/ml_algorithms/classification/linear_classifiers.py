6a598c6ac43d68743b9b1d178416ea9ddd6d3d8b
Krishna Priya <blacktheta20@gmail.com>
2026-05-04 20:47:47 +0530

    Optimize NumPy matrix operations for linear_classifiers.py (2026-05-04)

--- src/ml_algorithms/classification/linear_classifiers.py
+++ src/ml_algorithms/classification/linear_classifiers.py
@@ -60,4 +60,4 @@ class LinearSVMFromScratch:
         approx = np.dot(X, self.w) - self.b
         return np.where(np.sign(approx) <= 0, 0, 1)
 
-# Revision commit 2026-05-04 #12
+# Revision commit 2026-05-04 #14
