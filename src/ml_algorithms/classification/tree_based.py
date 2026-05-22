7c2fded2dfc2ab27be3d83a5ee5e7c59efd44aef
Krishna Priya <blacktheta20@gmail.com>
2026-05-22 16:28:59 +0530

    Improve hyperparameter handling in tree_based.py (2026-05-22)

--- src/ml_algorithms/classification/tree_based.py
+++ src/ml_algorithms/classification/tree_based.py
@@ -113,4 +113,4 @@ class RandomForestFromScratch:
         tree_preds = np.swapaxes(predictions, 0, 1)
         return np.array([np.bincount(p).argmax() for p in tree_preds])
 
-# Revision commit 2026-05-21 #8
+# Revision commit 2026-05-22 #6
