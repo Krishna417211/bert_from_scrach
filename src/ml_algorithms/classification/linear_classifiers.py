5c4535673720275d027d214199031f006a7faec3
Krishna Priya <blacktheta20@gmail.com>
2026-05-28 16:38:27 +0530

    Initial setup for linear_classifiers.py (2026-05-28)

--- src/ml_algorithms/classification/linear_classifiers.py
+++ src/ml_algorithms/classification/linear_classifiers.py
@@ -60,4 +60,4 @@ class LinearSVMFromScratch:
         approx = np.dot(X, self.w) - self.b
         return np.where(np.sign(approx) <= 0, 0, 1)
 
-# Revision commit 2026-05-28 #2
+# Revision commit 2026-05-28 #7
