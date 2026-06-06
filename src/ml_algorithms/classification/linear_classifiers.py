608ea0f0f4f4d64618c6e3a89698e006f14c61eb
Krishna Priya <blacktheta20@gmail.com>
2026-06-06 12:32:22 +0530

    Refactor and add type hints to linear_classifiers.py (2026-06-06)

--- src/ml_algorithms/classification/linear_classifiers.py
+++ src/ml_algorithms/classification/linear_classifiers.py
@@ -60,4 +60,4 @@ class LinearSVMFromScratch:
         approx = np.dot(X, self.w) - self.b
         return np.where(np.sign(approx) <= 0, 0, 1)
 
-# Revision commit 2026-06-05 #12
+# Revision commit 2026-06-06 #9
