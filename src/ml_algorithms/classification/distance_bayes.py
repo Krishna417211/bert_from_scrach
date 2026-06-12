a14d5b92768c8db3c92bcce2e22bcd88295cde04
Krishna Priya <blacktheta20@gmail.com>
2026-06-12 18:50:06 +0530

    Refactor and add type hints to distance_bayes.py (2026-06-12)

--- src/ml_algorithms/classification/distance_bayes.py
+++ src/ml_algorithms/classification/distance_bayes.py
@@ -55,4 +55,4 @@ class GaussianNaiveBayesFromScratch:
         denominator = np.sqrt(2 * np.pi * var)
         return numerator / denominator
 
-# Revision commit 2026-06-10 #8
+# Revision commit 2026-06-12 #10
