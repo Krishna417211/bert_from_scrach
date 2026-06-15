05cc308c07ebdf959eef5ace7416bf3644af6963
Krishna Priya <blacktheta20@gmail.com>
2026-06-15 10:03:01 +0530

    Refactor and add type hints to linear.py (2026-06-15)

--- src/ml_algorithms/regression/linear.py
+++ src/ml_algorithms/regression/linear.py
@@ -48,4 +48,4 @@ class PolynomialRegressionFromScratch:
         X_poly = self._transform(X)
         return self.linear_model.predict(X_poly)
 
-# Revision commit 2026-06-14 #3
+# Revision commit 2026-06-15 #1
