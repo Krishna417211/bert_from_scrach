a1cff5a8bbd134ebe6b18dce000ed6f4b398e180
Krishna Priya <blacktheta20@gmail.com>
2026-06-16 10:13:10 +0530

    Update synthetic test validation for linear_classifiers.py (2026-06-16)

--- src/ml_algorithms/classification/linear_classifiers.py
+++ src/ml_algorithms/classification/linear_classifiers.py
@@ -60,4 +60,4 @@ class LinearSVMFromScratch:
         approx = np.dot(X, self.w) - self.b
         return np.where(np.sign(approx) <= 0, 0, 1)
 
-# Revision commit 2026-06-10 #1
+# Revision commit 2026-06-16 #3
