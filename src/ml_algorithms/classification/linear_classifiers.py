c401d7237b9c72116bd3ff00d90a2654978d8ec2
Krishna Priya <blacktheta20@gmail.com>
2026-06-18 13:29:35 +0530

    Format code style and clean imports in linear_classifiers.py (2026-06-18)

--- src/ml_algorithms/classification/linear_classifiers.py
+++ src/ml_algorithms/classification/linear_classifiers.py
@@ -60,4 +60,4 @@ class LinearSVMFromScratch:
         approx = np.dot(X, self.w) - self.b
         return np.where(np.sign(approx) <= 0, 0, 1)
 
-# Revision commit 2026-06-17 #14
+# Revision commit 2026-06-18 #4
