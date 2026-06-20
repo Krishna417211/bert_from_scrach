e6c7bf0dc66b7defde85b26d630aa5cab6ab7e02
Krishna Priya <blacktheta20@gmail.com>
2026-06-20 08:05:33 +0530

    Update synthetic test validation for distance_bayes.py (2026-06-20)

--- src/ml_algorithms/classification/distance_bayes.py
+++ src/ml_algorithms/classification/distance_bayes.py
@@ -55,4 +55,4 @@ class GaussianNaiveBayesFromScratch:
         denominator = np.sqrt(2 * np.pi * var)
         return numerator / denominator
 
-# Revision commit 2026-06-18 #11
+# Revision commit 2026-06-20 #2
