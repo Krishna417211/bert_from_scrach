ae191ab4b37ad7c52406f12fb619f211f7e06ef0
Krishna Priya <blacktheta20@gmail.com>
2026-06-21 17:32:37 +0530

    Optimize NumPy matrix operations for registry.py (2026-06-21)

--- src/ml_algorithms/registry.py
+++ src/ml_algorithms/registry.py
@@ -50,4 +50,4 @@ def list_categories():
 def get_algorithms(category):
     return machine_learning_algorithms.get(category, [])
 
-# Revision commit 2026-06-20 #11
+# Revision commit 2026-06-21 #11
