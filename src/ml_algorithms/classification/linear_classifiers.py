5424814d1d8d15868c4eb023ebd0ab94a68d1329
Krishna Priya <blacktheta20@gmail.com>
2026-06-25 11:03:38 +0530

    Add docstrings and usage instructions in linear_classifiers.py (2026-06-25)

--- src/ml_algorithms/classification/linear_classifiers.py
+++ src/ml_algorithms/classification/linear_classifiers.py
@@ -60,4 +60,4 @@ class LinearSVMFromScratch:
         approx = np.dot(X, self.w) - self.b
         return np.where(np.sign(approx) <= 0, 0, 1)
 
-# Revision commit 2026-06-24 #12
+# Revision commit 2026-06-25 #4
