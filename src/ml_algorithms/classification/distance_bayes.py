05867ea1c84185ed2a560b0f401fc1a823c50ec0
Krishna Priya <blacktheta20@gmail.com>
2026-06-26 10:01:47 +0530

    Improve hyperparameter handling in distance_bayes.py (2026-06-26)

--- src/ml_algorithms/classification/distance_bayes.py
+++ src/ml_algorithms/classification/distance_bayes.py
@@ -55,4 +55,4 @@ class GaussianNaiveBayesFromScratch:
         denominator = np.sqrt(2 * np.pi * var)
         return numerator / denominator
 
-# Revision commit 2026-06-24 #9
+# Revision commit 2026-06-26 #1
