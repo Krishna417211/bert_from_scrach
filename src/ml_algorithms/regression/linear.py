523ff36c8124d76c73efdd48a2f6830bfff34be9
Krishna Priya <blacktheta20@gmail.com>
2026-07-01 19:56:27 +0530

    Implement core logic in linear.py (2026-07-01)

--- src/ml_algorithms/regression/linear.py
+++ src/ml_algorithms/regression/linear.py
@@ -48,4 +48,4 @@ class PolynomialRegressionFromScratch:
         X_poly = self._transform(X)
         return self.linear_model.predict(X_poly)
 
-# Revision commit 2026-06-30 #9
+# Revision commit 2026-07-01 #17
