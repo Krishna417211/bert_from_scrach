0b4b86d9b5a7119433f378db3a5ac3314244600b
Krishna Priya <blacktheta20@gmail.com>
2026-07-13 16:26:12 +0530

    Add docstrings and usage instructions in linear.py (2026-07-13)

--- src/ml_algorithms/regression/linear.py
+++ src/ml_algorithms/regression/linear.py
@@ -48,4 +48,4 @@ class PolynomialRegressionFromScratch:
         X_poly = self._transform(X)
         return self.linear_model.predict(X_poly)
 
-# Revision commit 2026-07-05 #9
+# Revision commit 2026-07-13 #7
