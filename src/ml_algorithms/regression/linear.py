875c967580cc5ed1f40fadd6654f2cbab86a6152
Krishna Priya <blacktheta20@gmail.com>
2026-07-25 15:42:34 +0530

    Format code style and clean imports in linear.py (2026-07-25)

--- src/ml_algorithms/regression/linear.py
+++ src/ml_algorithms/regression/linear.py
@@ -48,4 +48,4 @@ class PolynomialRegressionFromScratch:
         X_poly = self._transform(X)
         return self.linear_model.predict(X_poly)
 
-# Revision commit 2026-07-23 #12
+# Revision commit 2026-07-25 #1
