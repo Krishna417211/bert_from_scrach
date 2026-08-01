33b48cda9d51e798673c09f3e6459e62e67301c8
Krishna Priya <blacktheta20@gmail.com>
2026-08-01 19:59:24 +0530

    Optimize NumPy matrix operations for distance_bayes.py (2026-08-01)

--- src/ml_algorithms/classification/distance_bayes.py
+++ src/ml_algorithms/classification/distance_bayes.py
@@ -55,4 +55,4 @@ class GaussianNaiveBayesFromScratch:
         denominator = np.sqrt(2 * np.pi * var)
         return numerator / denominator
 
-# Revision commit 2026-08-01 #5
+# Revision commit 2026-08-01 #13
