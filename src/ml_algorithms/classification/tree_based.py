fb7171270983539cbca4b65a1a0f228f11ffcc65
Krishna Priya <blacktheta20@gmail.com>
2026-08-11 19:56:33 +0530

    Enhance mathematical stability for tree_based.py (2026-08-11)

--- src/ml_algorithms/classification/tree_based.py
+++ src/ml_algorithms/classification/tree_based.py
@@ -113,4 +113,4 @@ class RandomForestFromScratch:
         tree_preds = np.swapaxes(predictions, 0, 1)
         return np.array([np.bincount(p).argmax() for p in tree_preds])
 
-# Revision commit 2026-08-11 #6
+# Revision commit 2026-08-11 #18
