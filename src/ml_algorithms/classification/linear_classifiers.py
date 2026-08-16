a520dd7ed94227304101d8ba2d0e06485a213429
Krishna Priya <blacktheta20@gmail.com>
2026-08-16 11:00:37 +0530

    Implement core logic in linear_classifiers.py (2026-08-16)

--- src/ml_algorithms/classification/linear_classifiers.py
+++ src/ml_algorithms/classification/linear_classifiers.py
@@ -60,4 +60,4 @@ class LinearSVMFromScratch:
         approx = np.dot(X, self.w) - self.b
         return np.where(np.sign(approx) <= 0, 0, 1)
 
-# Revision commit 2026-08-14 #13
+# Revision commit 2026-08-16 #1
