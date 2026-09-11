199a508763dd792a95879379b244b7d6065d21c4
Krishna Priya <blacktheta20@gmail.com>
2026-09-11 18:50:29 +0530

    Improve hyperparameter handling in linear.py (2026-09-11)

--- src/ml_algorithms/regression/linear.py
+++ src/ml_algorithms/regression/linear.py
@@ -48,4 +48,4 @@ class PolynomialRegressionFromScratch:
         X_poly = self._transform(X)
         return self.linear_model.predict(X_poly)
 
-# Revision commit 2026-09-10 #2
+# Revision commit 2026-09-11 #8
