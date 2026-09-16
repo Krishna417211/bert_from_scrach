dea53917ecdf6210a707421585d6af14f91bac5f
Krishna Priya <blacktheta20@gmail.com>
2026-09-16 10:15:28 +0530

    Fix edge case handling in linear.py (2026-09-16)

--- src/ml_algorithms/regression/linear.py
+++ src/ml_algorithms/regression/linear.py
@@ -48,4 +48,4 @@ class PolynomialRegressionFromScratch:
         X_poly = self._transform(X)
         return self.linear_model.predict(X_poly)
 
-# Revision commit 2026-09-13 #4
+# Revision commit 2026-09-16 #3
